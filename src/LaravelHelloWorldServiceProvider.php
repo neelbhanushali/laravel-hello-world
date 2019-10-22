4fe38ccf88b711ab4d7d7347d845a1b73415f398
Neel Bhanushali <neal.bhanushali@relifems.com>
2019-10-22 13:47:08 +0530

    commands and service provider

---
+++ src/LaravelHelloWorldServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace NeelBhanushali;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelHelloWorldServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
